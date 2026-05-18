673aabf46bcdc8f7ff8fba9c1872ae4f1f3ea20b
hellio-kadmiel <kadmielhellio@gmail.com>
2026-05-18 15:11:34 -0300

    insert implemetation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -5,15 +5,14 @@ import model.dao.SellerDao;
 import moldel.entities.Department;
 import moldel.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.naming.Name;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static javax.swing.DropMode.INSERT;
 import static javax.swing.DropMode.ON;
 
 public class SellerDaoJDBC implements SellerDao {
@@ -25,7 +24,47 @@ public class SellerDaoJDBC implements SellerDao {
     }
     @Override
     public void insert(Seller obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO seller "
+                         +   "(Name,   Email, BirthDate, BaseSalary, DepartmentId) "
+                    +
+                        "VALUES "
+                   + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
 
+            st.setString(1,obj.getName());
+            st.setString(2,obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4,obj.getBaseSalary());
+            st.setInt(5,obj.getDepartment().getId());
+
+                int linhasAfetadas = st.executeUpdate();
+                if (linhasAfetadas > 0)
+                {
+                    ResultSet rs = st.getGeneratedKeys();
+                    if (rs.next())
+                    {
+                        int id = rs.getInt(1);
+                        obj.setId(id);
+
+                }
+                    DB.closeResultSet(rs);
+
+        }
+                else {
+                    throw new DbException("Erro inesperado ao inserir os dados");
+                }
+
+
+    }
+        catch (SQLException e) {
+            throw new DbException("Erro inesperado ao inserir os dados");
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
